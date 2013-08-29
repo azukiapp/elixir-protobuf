59f09c232703fd3785e1b3519c6bba6dec3fee39
Everton Ribeiro <nuxlli@gmail.com>
2013-08-28 21:09:00 -0300

    Fixing bug in string field in submsg and repeated strings;

--- lib/protobuf/decoder.ex
+++ lib/protobuf/decoder.ex
@@ -3,27 +3,46 @@ defmodule Protobuf.Decoder do
 
   # Decode with record/module
   def decode(bytes, record) do
-    fix_values :gpb.decode_msg(bytes, record, record.defs)
+    fix_msg :gpb.decode_msg(bytes, record, record.defs)
   end
 
-  defp fix_values(msg) do
+  def varint(bytes) do
+    :gpb.decode_varint(bytes)
+  end
+
+  defp fix_msg(msg) do
     Enum.reduce(msg.__record__(:fields), msg, fn
       {field, default}, msg ->
         value = apply(msg, field, [])
         if value == :undefined do
           apply(msg, field, [default])
         else
-          case msg.defs(:field, field) do
-            :field[type: :string] ->
-              apply(msg, field, [ :unicode.characters_to_binary(value) ])
-            _ ->
-              msg
-          end
+          fix_field(value, msg, msg.defs(:field, field))
         end
     end)
   end
 
-  def varint(bytes) do
-    :gpb.decode_varint(bytes)
+  defp fix_field(value, msg, :field[name: field, type: type, occurrence: occurrence]) do
+    case {occurrence, type} do
+      {:repeated, _} ->
+        value = lc v inlist value, do: fix_value(type, v)
+        apply(msg, field, [value])
+      {_, :string}   ->
+        apply(msg, field, [fix_value(type, value)])
+      {_, {:msg, _}} ->
+        apply(msg, field, [fix_value(type, value)])
+      _ ->
+        msg
+    end
   end
+
+  defp fix_value(:string, value) do
+    :unicode.characters_to_binary(value)
+  end
+
+  defp fix_value({:msg, _}, value) do
+    fix_msg(value)
+  end
+
+  defp fix_value(_, value), do: value
 end
--- test/protobuf/decoder_test.exs
+++ test/protobuf/decoder_test.exs
@@ -12,12 +12,30 @@ defmodule Protobuf.Decoder.Test do
     assert {mod.Msg, nil, 150} == D.decode(<<16, 150, 1>>, mod.Msg)
   end
 
+  test "fix repeated values" do
+    mod = def_proto_module "message Msg {
+      repeated string f1 = 1;
+    }"
+
+    bytes = <<10, 3, 102, 111, 111, 10, 3, 98, 97, 114>>
+    assert {mod.Msg, ["foo", "bar"] } == D.decode(bytes, mod.Msg)
+  end
+
   test "fixing string values" do
     mod = def_proto_module "message Msg {
       required string f1 = 1;
+
+      message SubMsg {
+        required string f1 = 1;
+      }
+
+      optional SubMsg f2 = 2;
     }"
 
     bytes = <<10,11,?a,?b,?c,?\303,?\245,?\303,?\244,?\303,?\266,?\317,?\276>>
-    assert {mod.Msg, "abcåäöϾ"} == D.decode(bytes, mod.Msg)
+    assert {mod.Msg, "abcåäöϾ", nil} == D.decode(bytes, mod.Msg)
+
+    bytes = <<10, 1, 97, 18, 5, 10, 3, 97, 98, 99>>
+    assert {mod.Msg, "a", {mod.Msg.SubMsg, "abc"}} == D.decode(bytes, mod.Msg)
   end
 end
